b8145cee911d93afe43fa52d516988a95dbb4249
Abhishek Kadam <abhikadam393@gmail.com>
2023-06-05 18:59:13 +0530

    Modified files to change the workspace for docker.sock issue

--- jenkins/build/build-jarfile-1.sh
+++ jenkins/build/build-jarfile-1.sh
@@ -2,5 +2,7 @@
 
 echo "******Building Jar File******"
 
-docker run --rm  -v $PWD/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven  "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm  -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven  "$@"
 
--- jenkins/test/test-mvn-build-3.sh
+++ jenkins/test/test-mvn-build-3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/java-app/:/app -v /root/.m2:/root/.m2/ -w /app maven "$@"
+echo "*******************************"
+echo "****** Testing Our Build ******"
+echo "*******************************"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2/ -w /app maven "$@"
